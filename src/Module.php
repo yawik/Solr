a796c59119004b6e105922ebf806014c2fcb3441
Mathias Gelhausen <gelhausen@cross-solution.de>
2020-06-12 17:57:21 +0200

    Release 0.9.4

--- src/Module.php
+++ src/Module.php
@@ -26,7 +26,7 @@ class Module implements ConsoleUsageProviderInterface,VersionProviderInterface
 {
     use VersionProviderTrait;
 
-    const VERSION = '0.9.3';
+    const VERSION = '0.9.4';
 
     public function getConsoleUsage(AdapterInterface $console)
     {
